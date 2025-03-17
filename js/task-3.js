d786ffd58444847eeb9ad2cb8519303c38e7b4aa
caglayan <caglayanustaa@gmail.com>
2025-03-17 19:35:53 +0300

    update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,10 @@
 function getElementWidth(content, padding, border) {
-    let result;
-
-    let newContent = content.split("px")[0]
-    let newPadding = padding.split('px')[0];
-    let newBorder = border.split('px')[0];
     
-    result = Number.parseFloat(newContent) + Number.parseFloat(newPadding) * 2 + Number.parseFloat(newBorder) * 2
-
-    return result
+    const newContent = content.split("px")[0]
+    const newPadding = padding.split('px')[0];
+    const newBorder = border.split('px')[0];
+    
+    return Number.parseFloat(newContent) + Number.parseFloat(newPadding) * 2 + Number.parseFloat(newBorder) * 2
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
